f27c4ba8acf37986a9d8902a3a2300f686465291
Steve <bevets87@gmail.com>
2018-01-31 09:12:20 -0500

    refactored cor pattern

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # **Memory-Game**
-[https://bevets87.github.io/Memory-Game/](https://bevets87.github.io/Memory-Game/)
+[]()
 
-#### Classic memory game, built in es6 JavaScript, with the MVC pattern.  
+#### Classic memory game, built in es6   
--- src/app/controller.js
+++ src/app/controller.js
@@ -1,3 +1,5 @@
+import { curry } from 'lodash/fp'
+
 class Controller {
   constructor (model, view) {
     const self = this;
@@ -5,135 +7,127 @@ class Controller {
     self.model = model;
     self.view = view;
 
-    self.handleUserCardPick = this.setResponsibilityChain(this.addPick, this.isMatch, this.resetGame)
+    self.handleUserCardPick = this._setNextChain(
+      this._validatePickedCard,
+      this.setPickedCard,
+      this._checkPairedCardsForMatch,
+      this.setPairedCards,
+      this.resetCards
+    )
 
     self.view.bind('startGame', () => {
-      self.startGame({...self.model.state})
+      self.setStartGame({
+        ...self.model.state
+      })
     })
 
     self.view.bind('userCardPick', pick_id => {
-      self.handleUserCardPick({...self.model.state, picked_card: self.model.state.cards.filter(card => card.id === pick_id)})
+      self.handleUserCardPick({
+        ...self.model.state,
+        picked_card: self.model.state.cards.filter(card => card.id == pick_id),
+      })
     })
 
   }
 
-  startGame = ({ cards }) => {
+  setStartGame = ({ cards }) => {
+    return this.model.setState({
+      cards: this._shuffleCards(cards)
+    }, ({ cards }) => {
+      this.view.render('displayCards', {cards: cards})
+    })
+  }
+
+  setPickedCard = curry((next, { pickedCard, pickCount, cards }) => {
+    const newState = this.model.setState({
+      cards: cards.map(card => pickedCard && card.id === pickedCard.id ? pickedCard : card),
+      pickCount: pickCount
+    }, () => {
+      this.view.render('rotateCard', {card: pickedCard ? pickedCard : null})
+    })
+    if (newState.pickCount === 2) {
+      next({...newState})
+    }
+  })
+
+  setPairedCards = curry((next, { cardOne, cardTwo, cards, matchCount }) => {
+    const newState = this.model.setState({
+      cards: cards
+      .map(card => card.id === cardOne.id ? cardOne : card)
+      .map(card => card.id === cardTwo.id ? cardTwo : card),
+      matchCount: matchCount,
+    }, () => {
+      setTimeout(() => {
+        this.view.render('rotateCard', {card: !cardOne.matched ? cardOne : null})
+        this.view.render('rotateCard', {card: !cardTwo.matched ? cardTwo : null})
+        this.model.setState({pickCount: 0})
+      }, 500)
+    })
+    if (newState.matchCount === 15) {
+      next({...newState})
+    }
+  })
+
+  resetCards = ({ cards }) => {
+    setTimeout(() => {
+      cards.forEach(card => {
+        this.view.render('rotateCard', {card: card})
+        this.view.render('removeCard', {card: card})
+      })
+    }, 500)
     this.model.setState({
-      cards: this.shuffleCards(cards)
-    }, newState => {
-      this.view.render('displayCards', {cards: newState.cards})
+      cards: cards.map(card => {card.matched = false;card.picked = false;return card}),
+      pickCount: 0,
+      matchCount: 0
+    }, ({ cards }) => {
+      setTimeout(() => {
+        this.view.render('displayCards', {cards: this._shuffleCards(cards)})
+      }, 1000)
     })
   }
 
-  /* Chain Of Responsibility Design Pattern */
-  setResponsibilityChain = (...tasks) => {
-    let chain = [];
-
-    tasks
-    .map(task => task())
-    .reduce((acc, curr) => {
-      acc.setNext(curr)
-      chain.push(acc)
-      return curr;
-    })
-
-    let taskOne = chain.shift()
-
-    return (args) => {
-      taskOne.execute(args)
-    }
-  }
-
-  addPick = () => ({
-    next:null,
-    setNext: nextHandler => {
-      this.addPick.next = nextHandler
-    },
-    execute: ({ pickCount, cards, picked_card }) => {
-      if (pickCount < 2) {
-        let [pickedCard, newPickCount ] = picked_card.reduce((arr, pickedCard) => {
-          if (!pickedCard.picked && !pickedCard.matched) {
-            pickedCard.picked = true;
-            pickCount++;
-          } else {
-            pickedCard = null;
-          }
-            arr.push(pickedCard, pickCount)
-            return arr
-        },[])
-
-        this.model.setState({
-          cards: cards.map(card => pickedCard && card.id === pickedCard.id ? pickedCard : card),
-          pickCount: newPickCount
-        }, newState => {
-          this.view.render('rotateCard', {card: pickedCard ? pickedCard : null})
-          if (newState.pickCount === 2) {
-            this.addPick.next.execute({...newState})
-          }
-        })
-
+  _validatePickedCard = curry((next, { pickCount, picked_card, cards }) => {
+    if (pickCount < 2) {
+      const nextState = picked_card.reduce((acc, pickedCard) => {
+      if (!pickedCard.picked && !pickedCard.matched) {
+        pickedCard.picked = true;
+        pickCount++
+      } else {
+          pickedCard = null;
       }
+        acc.pickedCard = pickedCard
+        return acc
+      },{cards: cards})
+      next({...nextState, pickCount})
     }
   })
 
-  isMatch = () => ({
-    next: null,
-    setNext: nextHandler => {
-      this.isMatch.next = nextHandler
-    },
-    execute: ({ cards, matchCount }) => {
-
-      let [ newMatchCount, cardOne, cardTwo ] = cards
-      .filter(card => card.picked)
-      .reduce((cardOne, cardTwo) => {
-        cardOne.picked = false;
-        cardTwo.picked = false;
-        if (cardOne.name === cardTwo.name) {
-          cardOne.matched = true;
-          cardTwo.matched = true;
-          matchCount++;
-        }
-        return [matchCount, cardOne, cardTwo]
-      })
-
-      this.model.setState({
-        cards: cards
-        .map(card => card.id === cardOne.id ? cardOne : card)
-        .map(card => card.id === cardTwo.id ? cardTwo : card),
-        matchCount: newMatchCount,
-      }, newState => {
-        setTimeout(() => {
-          this.view.render('rotateCard', {card: !cardOne.matched ? cardOne : null})
-          this.view.render('rotateCard', {card: !cardTwo.matched ? cardTwo : null})
-          this.model.setState({pickCount: 0})
-        }, 500)
-        if (newState.matchCount === 15) {
-          this.isMatch.next.execute({cards: newState.cards.slice()})
-        }
-      })
-    }
+  _checkPairedCardsForMatch = curry((next, { cards, matchCount }) => {
+    let [ cardOne, cardTwo ] = cards
+    .filter(card => card.picked)
+    .reduce((cardOne, cardTwo) => {
+      cardOne.picked = false;
+      cardTwo.picked = false;
+      if (cardOne.name === cardTwo.name) {
+        cardOne.matched = true;
+        cardTwo.matched = true;
+        matchCount++;
+      }
+      return [ cardOne, cardTwo ]
+    })
+    next({cardOne, cardTwo, cards, matchCount})
   })
 
-  resetGame = () => ({
-    next: null,
-    setNext: nextHandler => {
-      this.resetGame.next = nextHandler
-    },
-    execute: ({ cards }) => {
-      cards.forEach(card => {this.view.render('removeCard', {card: card})})
-      setTimeout(() => {cards.forEach(card => {this.view.render('rotateCard', {card: card})})},500)
+  _setNextChain = (...ops) => {
+    return ops
+    .reverse()
+    .reduce((lastOp, prevOp) => {
+      let curriedPrev = prevOp(lastOp)
+      return curriedPrev
+    })
+  }
 
-      this.model.setState({
-        cards: cards.map(card => {card.matched = false;card.picked = false;return card}),
-        pickCount: 0,
-        matchCount: 0
-      }, newState => {
-        this.view.render('displayCards', {cards:this.shuffleCards(newState.cards)})
-      })
-    }
-  })
-
-  shuffleCards = cards => {
+  _shuffleCards = cards => {
     cards = cards.slice()
     var shuffled = [];
     while (cards.length > 0) {
@@ -143,6 +137,8 @@ class Controller {
     }
     return shuffled;
   }
+
+
 }
 
 export default Controller
